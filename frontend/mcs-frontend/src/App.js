86d35c0275545d0900d168aa2f6dc3e943fbbd86
qapng <quanganhphucnguyen@gmail.com>
2024-04-30 17:09:21 +1000

    add back button and mui icons

--- frontend/mcs-frontend/src/App.js
+++ frontend/mcs-frontend/src/App.js
@@ -1,9 +1,11 @@
+import BackButton from "./components/BackButton";
 import NavBar from "./components/TopNavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <BackButton text="Back" />
     </>
   );
 }
