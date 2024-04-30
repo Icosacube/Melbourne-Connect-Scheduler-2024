745c51a4718e26ee9f7510c064601378dfe6ce76
qapng <quanganhphucnguyen@gmail.com>
2024-04-30 22:20:14 +1000

    add speaker title and profile header

--- frontend/mcs-frontend/src/App.js
+++ frontend/mcs-frontend/src/App.js
@@ -1,20 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import BackButton from "./components/BackButton";
-import NavBar from "./components/TopNavBar";
-import SideNavBar from "./components/SideNavBar";
 import EventTitle from "./components/EventTitle";
+import ProfileHeader from "./components/ProfileHeader";
+import SideNavBar from "./components/SideNavBar";
+import SpeakerTitle from "./components/SpeakerTitle";
+import NavBar from "./components/TopNavBar";
 
 function App() {
+  const tags = ["CEO", "Justice", "Dark Knight", "Vigilante", "Billionaire"];
   return (
     <>
       <NavBar />
       <SideNavBar />
-      <Box className="flex justify-center items-center h-screen">
+      <Box className="flex justify-center items-center h-screen space-x-6">
         <BackButton text="Back" />
         <EventTitle
-          eventName="Event Title"
+          eventName="Birthday Party"
           dateTime="30/04/2024 16:20"
-          guestName="Guest Name"
+          guestName="Bruce Wayne"
+        />
+
+        <SpeakerTitle firstname="Bruce" lastname="Wayne">
+          <Typography>CEO of Wayne Enterprises</Typography>
+          <Typography>Hammer of Justice</Typography>
+          <Typography>Dark Knight</Typography>
+        </SpeakerTitle>
+
+        <ProfileHeader
+          title="Mr"
+          firstname="Bruce"
+          lastname="Wayne"
+          organisation="Wayne Enterprise"
+          tags={tags}
         />
       </Box>
     </>
---
+++ frontend/mcs-frontend/src/components/ProfileHeader/ProfileHeader.jsx
@@ -0,0 +1,36 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  Stack,
+  Typography,
+  Chip,
+  Box,
+} from "@mui/material";
+import React from "react";
+
+function ProfileHeader({ title, firstname, lastname, organisation, tags }) {
+  return (
+    <Card>
+      <CardContent className="space-x-28 bg-blue-400 min-w-96 text-white flex flex-row ">
+        <Stack direction="row" spacing={2} className="place-items-center">
+          <Avatar className="size-20">A</Avatar>
+          <Stack>
+            <Typography className="text-2xl">{title}</Typography>
+            <Typography className="text-4xl ">
+              {firstname} {lastname}
+            </Typography>
+            <Typography className="text-lg ">{organisation}</Typography>
+          </Stack>
+        </Stack>
+        <Stack direction="row" className="flex flex-wrap max-w-40 ">
+          {tags.map((tag, i) => {
+            return <Chip label={tag} className="text-white" />;
+          })}
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ProfileHeader;
