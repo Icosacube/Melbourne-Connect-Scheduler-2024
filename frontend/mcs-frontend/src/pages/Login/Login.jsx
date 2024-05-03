da2b8ca9fe20012abd55641b81e63369e52a8c45
qapng <quanganhphucnguyen@gmail.com>
2024-05-03 16:39:16 +1000

    add login page

--- frontend/mcs-frontend/src/pages/Login/Login.jsx
+++ frontend/mcs-frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,20 @@
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Stack component="form" className="space-y-8">
+      <Typography variant="h4">Login</Typography>
+      <TextField required id="outlined-required" label="Username" />
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+      />
+      <Button variant="contained">Login</Button>
+    </Stack>
+  );
 }
 
 export default Login;
