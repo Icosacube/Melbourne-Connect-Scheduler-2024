e4db32d88d2651750460708ba09a2dd25d9352ea
Kia Sen Tan <kiat@student.unimelb.edu.au>
2024-05-09 21:17:25 +1000

    Top bar now functions as navigation (broken links still exist)

--- frontend/mcs-frontend/src/pages/Layout/Layout.jsx
+++ frontend/mcs-frontend/src/pages/Layout/Layout.jsx
@@ -1,5 +1,5 @@
-import { Box, Divider } from "@mui/material";
-import { Link, Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
 import { TopNavBar } from "../../components";
 
 function Layout() {
@@ -8,18 +8,7 @@ function Layout() {
       <TopNavBar />
       <Box className="h-screen flex items-center justify-center space-x-10">
         <Outlet />
-        {/* Temporary navigation */}
-        <Box>
-          <Link to="/dashboard">Dashboard</Link>
-          <Divider />
-          <Link to="/event">Event</Link>
-          <Divider />
-          <Link to="/speaker">Speaker</Link>
-          <Divider />
-          <Link to="/login">Login</Link>
-          <Divider />
-          <Link to="/components">Components</Link>
-        </Box>
+        
       </Box>
     </>
   );
