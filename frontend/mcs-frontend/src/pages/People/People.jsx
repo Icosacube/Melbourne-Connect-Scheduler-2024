dc1013b3ca0ba835747588aea3a5bbef5a5c18a3
qapng <quanganhphucnguyen@gmail.com>
2024-05-12 15:41:48 +1000

    add profile page and routes

--- frontend/mcs-frontend/src/pages/People/People.jsx
+++ frontend/mcs-frontend/src/pages/People/People.jsx
@@ -4,17 +4,20 @@ import { Box, Divider, Typography } from "@mui/material";
 import { BackButton, ProfileCard } from "../../components";
 import EditCalendar from "@mui/icons-material/EditCalendar";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import { NavLink } from "react-router-dom";
 function People() {
   return (
     <Container className="container flex space-x-10">
-      <Box className="w-2/3">
+      <Box className="w-2/3 space-y-4">
         <Box className="flex justify-between">
           <BackButton text="Back" />
           <EditCalendar fontSize="large" />
         </Box>
         <Box className="grid grid-cols-2 gap-5 p-5 overflow-scroll max-h-[45rem]">
           {Array.from(Array(20)).map((_, index) => (
-            <ProfileCard firstname="John" lastname="Stevens" roletag="CEO" />
+            <NavLink to={`/people/${index}`} key={index}>
+              <ProfileCard firstname="John" lastname="Stevens" roletag="CEO" />
+            </NavLink>
           ))}
         </Box>
       </Box>
