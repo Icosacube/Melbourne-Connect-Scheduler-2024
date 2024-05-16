f0d6bb022aa1f824454a874d3db38851871d65e6
qapng <quanganhphucnguyen@gmail.com>
2024-05-16 15:40:40 +1000

    change to theme color

--- frontend/mcs-frontend/src/components/BackButton/BackButton.jsx
+++ frontend/mcs-frontend/src/components/BackButton/BackButton.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from 'react-router-dom';
+import { yellow } from '@mui/material/colors';
+import { Typography } from '@mui/material';
 
 function BackButton({ text }) {
   const navigate = useNavigate();
   return (
     <Button
-      startIcon={<ArrowBackIcon />}
+      startIcon={<ArrowBackIcon sx={{ color: yellow[900] }} />}
       onClick={() => {
         navigate(-1);
       }}>
-      {text}
+      <Typography sx={{ color: yellow[900] }}>{text}</Typography>
     </Button>
   );
 }
