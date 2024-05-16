d54891b93027ca92c1d8ac6317613f359b4385dc
qapng <quanganhphucnguyen@gmail.com>
2024-05-16 15:37:33 +1000

    add back button functionality

--- frontend/mcs-frontend/src/components/BackButton/BackButton.jsx
+++ frontend/mcs-frontend/src/components/BackButton/BackButton.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-import Button from "@mui/material/Button";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import React from 'react';
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
 
 function BackButton({ text }) {
-  return <Button startIcon={<ArrowBackIcon />}>{text}</Button>;
+  const navigate = useNavigate();
+  return (
+    <Button
+      startIcon={<ArrowBackIcon />}
+      onClick={() => {
+        navigate(-1);
+      }}>
+      {text}
+    </Button>
+  );
 }
 
 export default BackButton;
