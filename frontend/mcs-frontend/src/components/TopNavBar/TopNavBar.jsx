1a7ce4036317ef8549ed7eb5c104d981436228f7
qapng <quanganhphucnguyen@gmail.com>
2024-05-16 13:11:49 +1000

    improve navbar ui

--- frontend/mcs-frontend/src/components/TopNavBar/TopNavBar.jsx
+++ frontend/mcs-frontend/src/components/TopNavBar/TopNavBar.jsx
@@ -1,35 +1,38 @@
-import { Avatar } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Toolbar from "@mui/material/Toolbar";
-import * as React from "react";
-import { ReactComponent as Logo} from "../../Hex Logo (Colour).svg"
+import { Avatar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import * as React from 'react';
+import { ReactComponent as Logo } from '../../Hex Logo (Colour).svg';
+import { useLocation } from 'react-router-dom';
 
 const pages = [
   {
-    name: "Events",
-    url: "/events",
+    name: 'Events',
+    url: '/events'
   },
   {
-    name: "Trips",
-    url: "/trips",
+    name: 'Trips',
+    url: '/trips'
   },
   {
-    name: "People",
-    url: "/people",
+    name: 'People',
+    url: '/people'
   },
   {
-    name: "Finance",
-    url: "/finance",
+    name: 'Finance',
+    url: '/finance'
   },
   {
-    name: "Components",
-    url: "/components",
-  },
+    name: 'Components',
+    url: '/components'
+  }
 ];
 
 function TopNavBar() {
+  const curPath = useLocation().pathname;
+
   return (
     <AppBar position="sticky">
       <Toolbar className="bg-primary">
@@ -40,12 +43,16 @@ function TopNavBar() {
         </Box>
         <Box className="flex justify-end grow space-x-10">
           {pages.map((page) => (
-            <Button key={page.name} className="text-black " href={page.url}>
-              {page.name}
+            <Button key={page.name} href={page.url}>
+              <Typography
+                variant="h6"
+                className={`text-yellow-700 font-bold text-xl ${curPath.includes(page.url) ? 'underline decoration-4' : ''}`}>
+                {page.name}
+              </Typography>
             </Button>
           ))}
 
-          <Avatar className="size-8">A</Avatar>
+          <Avatar className="size-14"></Avatar>
         </Box>
       </Toolbar>
     </AppBar>
