be127b6975fceaea6e38026d5bd5c9f3fc78bed8
qapng <quanganhphucnguyen@gmail.com>
2024-05-19 14:17:29 +1000

    extract widgets

--- frontend/mcs-frontend/src/pages/Event/Events.jsx
+++ frontend/mcs-frontend/src/pages/Event/Events.jsx
@@ -1,6 +1,7 @@
-import { Box, Button, Card, CardContent, Typography } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import React from 'react';
 import EventsTable from './EventsTable';
+import EventsWidgets from './EventsWidgets';
 
 function Events() {
   const [open, setOpen] = React.useState(false);
@@ -16,7 +17,7 @@ function Events() {
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
-    p: 4,
+    p: 4
   };
 
   return (
@@ -26,61 +27,7 @@ function Events() {
           {' '}
           + Add Event
         </Button>
-        <Box className="flex space-x-3 w-full">
-          <Card className="w-1/4 p-3 rounded-2xl">
-            <CardContent>
-              <Typography variant="h7" className="font-medium text-gray-500 ">
-                New Events
-              </Typography>
-              <Box className="flex justify-between mt-4">
-                <Typography variant="h4" className="font-medium ">
-                  22
-                </Typography>
-                <Typography variant="h4">🎊</Typography>
-              </Box>
-            </CardContent>
-          </Card>
-          <Card className="w-1/4 p-3 rounded-2xl">
-            <CardContent>
-              <Typography variant="h7" className="font-medium text-gray-500 ">
-                Events Completed
-              </Typography>
-              <Box className="flex justify-between mt-4">
-                <Typography variant="h4" className="font-medium ">
-                  12
-                </Typography>
-                <Typography variant="h4">✅</Typography>
-              </Box>
-            </CardContent>
-          </Card>
-          <Card className="w-1/4 p-3 rounded-2xl">
-            <CardContent>
-              <Typography variant="h7" className="font-medium text-gray-500 ">
-                Events Happening Soon
-              </Typography>
-              <Box className="flex justify-between mt-4">
-                <Typography variant="h4" className="font-medium ">
-                  7
-                </Typography>
-                <Typography variant="h4">⚠️</Typography>
-              </Box>
-            </CardContent>
-          </Card>
-          <Card className="w-1/4 p-3 rounded-2xl">
-            <CardContent>
-              <Typography variant="h7" className="font-medium text-gray-500 ">
-                Upcoming Spend
-              </Typography>
-              <Box className="flex justify-between mt-4">
-                <Typography variant="h4" className="font-medium ">
-                  $468.40
-                </Typography>
-                <Typography variant="h4">💵</Typography>
-              </Box>
-            </CardContent>
-          </Card>
-
-        </Box>
+        <EventsWidgets />
       </Box>
       <Box className="w-full bg-white shadow-md">
         <EventsTable />
