a047625b8086574ea26d161a69baa3aec28eed7b
qapng <quanganhphucnguyen@gmail.com>
2024-05-19 21:08:45 +1000

    add people overview page

--- frontend/mcs-frontend/src/pages/People/People.jsx
+++ frontend/mcs-frontend/src/pages/People/People.jsx
@@ -1,33 +1,43 @@
-import EditCalendar from "@mui/icons-material/EditCalendar";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import { Box, Divider, Typography } from "@mui/material";
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { BackButton, ProfileCard } from "../../components";
+import { Box, Button } from '@mui/material';
+import React from 'react';
+import PeopleTable from './PeopleTable';
+import PeopleWidgets from './PeopleWidgets';
+import AddCircleOutlineOutlined from '@mui/icons-material/AddCircleOutlineOutlined';
 function People() {
   return (
-    <Box className=" flex space-x-10">
-      <Box className="w-2/3 space-y-4">
-        <Box className="flex justify-between">
-          <BackButton text="Back" />
-          <EditCalendar fontSize="large" />
-        </Box>
-        <Box className="grid grid-cols-2 gap-5 p-5 overflow-scroll max-h-[45rem]">
-          {Array.from(Array(20)).map((_, index) => (
-            <NavLink to={`/people/${index}`} key={index}>
-              <ProfileCard firstname="John" lastname="Stevens" roletag="CEO" />
-            </NavLink>
-          ))}
-        </Box>
-      </Box>
-      <Divider orientation="vertical" flexItem />
-      <Box className="w-1/3 space-y-5">
-        <FilterListIcon fontSize="large" />
-        <Typography variant="h6">Academic Domain</Typography>
-        <Typography variant="body1"></Typography>
-        <Typography variant="h6">Some other field</Typography>
-      </Box>
+    <Box className="space-y-8 flex flex-col">
+      <Button
+        startIcon={<AddCircleOutlineOutlined />}
+        variant="contained"
+        className="self-end bg-primary hover:bg-accent hover:text-black">
+        Add New
+      </Button>
+
+      <PeopleWidgets />
+      <PeopleTable />
     </Box>
+    // <Box className=" flex space-x-10">
+    //   <Box className="w-2/3 space-y-4">
+    //     <Box className="flex justify-between">
+    //       <BackButton text="Back" />
+    //       <EditCalendar fontSize="large" />
+    //     </Box>
+    //     <Box className="grid grid-cols-2 gap-5 p-5 overflow-scroll max-h-[45rem]">
+    //       {Array.from(Array(20)).map((_, index) => (
+    //         <NavLink to={`/people/${index}`} key={index}>
+    //           <ProfileCard firstname="John" lastname="Stevens" roletag="CEO" />
+    //         </NavLink>
+    //       ))}
+    //     </Box>
+    //   </Box>
+    //   <Divider orientation="vertical" flexItem />
+    //   <Box className="w-1/3 space-y-5">
+    //     <FilterListIcon fontSize="large" />
+    //     <Typography variant="h6">Academic Domain</Typography>
+    //     <Typography variant="body1"></Typography>
+    //     <Typography variant="h6">Some other field</Typography>
+    //   </Box>
+    // </Box>
   );
 }
 
