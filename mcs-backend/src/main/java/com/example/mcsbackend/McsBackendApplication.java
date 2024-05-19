74843f3adab11d224d7cbb84f59c13a42cf35799
zheyuanWill <148622683+zheyuanWill@users.noreply.github.com>
2024-05-19 18:51:00 +1000

    fix crash branch

--- mcs-backend/src/main/java/com/example/mcsbackend/McsBackendApplication.java
+++ mcs-backend/src/main/java/com/example/mcsbackend/McsBackendApplication.java
@@ -2,15 +2,17 @@ package com.example.mcsbackend;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@EnableJpaAuditing
 @SpringBootApplication
 public class McsBackendApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello Wrld!";
     }
 
     public static void main(String[] args) {
