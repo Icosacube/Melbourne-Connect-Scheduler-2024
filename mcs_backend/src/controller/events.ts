b6a631e43211cb5652175abc6369bc46a2607208
Kia Sen Tan <kiat@student.unimelb.edu.au>
2024-06-25 14:30:14 +1000

    getter for events

--- mcs_backend/src/controller/events.ts
+++ mcs_backend/src/controller/events.ts
@@ -1,6 +1,19 @@
-
-
+const events: any[] = []
+/**
+ * Handles (main?) Event requests
+ */
 module.exports = function(app:any) {
-    app.get(); // placeholder
     
+    // Get all events
+    app.get("/event", async (req:any, res:any, next:any) => {
+        console.log("get all events");
+        try {
+            res.sendStatus(200);
+            res.json(events);
+        } catch (err) {
+            res.sendStatus(404);
+            console.error(err);
+        }
+    })
+
 }
--- mcs_backend/src/index.ts
+++ mcs_backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 
 const app = express();
+
+require('./controller/events')(app);
+
 app.listen(4000, () => {
   console.log(`server running on port 4000`);
 });
