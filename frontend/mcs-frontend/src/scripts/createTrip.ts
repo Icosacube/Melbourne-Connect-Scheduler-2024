a3d3f5e1cd88ba6cf40bea0df0975a9d5672218b
pH7uang <pH7uang@gmail.com>
2024-07-29 17:49:56 +1000

    trip creation DayJS issue fixed

--- frontend/mcs-frontend/src/scripts/createTrip.ts
+++ frontend/mcs-frontend/src/scripts/createTrip.ts
@@ -5,8 +5,11 @@ import { Trip } from '../types/types';
 export default async function createTrip(trip: Trip) {
   console.log('POSTing to ' + process.env.REACT_APP_BACKEND_URL);
   const speaker_id = trip.GuestSpeaker![0];
+  // formatting DayJS to String
+  const new_trip = { ...trip, StartDate: trip.StartDate?.format('YYYY-MM-DD'),  EndDate: trip.EndDate?.format('YYYY-MM-DD')}
+  //console.log(new_trip)
   const res = await axios.post(process.env.REACT_APP_BACKEND_URL + '/trip/' + speaker_id, 
-      trip
+    new_trip
   );
   // if it doesn't work an error message should pop up for user feedback
   return res.status
