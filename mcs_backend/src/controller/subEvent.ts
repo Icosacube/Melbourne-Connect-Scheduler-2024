c5b58386b652c804895e533d670955f9a304d3cb
Keahna <khansenferna@student.unimelb.edu.au>
2024-08-04 16:51:29 +1000

    feat: added GET endpoint to fetch a specific subevent

--- mcs_backend/src/controller/subEvent.ts
+++ mcs_backend/src/controller/subEvent.ts
@@ -9,40 +9,61 @@ import {
 } from '../models/airtable';
 
 const router = express.Router();
-const SubEventTable = String(process.env.SUBEVENT)
+const SubeventTable = String(process.env.SUBEVENT)
 
 // route to get all subevents
-router.get('/sub_events', async (req, res) => {
+router.get('/subevents', async (req, res) => {
     try {
         // fetch subevents
-        const subEvents = await getTable(SubEventTable, "");
+        const subevents = await getTable(SubeventTable, "");
         // format subevents
-        const formattedSubEvents: { id: string, fields: any }[] = [];
-        subEvents.forEach((fields, id) => {
+        const formattedSubevents: { id: string, fields: any }[] = [];
+        subevents.forEach((fields, id) => {
             const plainFields = Object.fromEntries(fields);
-            formattedSubEvents.push({ id, fields: plainFields });
+            formattedSubevents.push({ id, fields: plainFields });
         });
-        res.json(formattedSubEvents);
+        res.json(formattedSubevents);
     } catch (error) {
+        console.error("Error Fetching Subevents:", error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
 // route to get subevents for a specific event
-router.get('/sub_events/:event_id', async (req, res) => {
+router.get('/subevents/:event_id', async (req, res) => {
     const { event_id } = req.params;
     try {
-        const subEvents = await getTable(SubEventTable, event_id);
+        const subevents = await getTable(SubeventTable, event_id);
 
-        const formattedSubEvents: { id: string, fields: any }[] = [];
-        subEvents.forEach((fields, id) => {
+        const formattedSubevents: { id: string, fields: any }[] = [];
+        subevents.forEach((fields, id) => {
             const plainFields = Object.fromEntries(fields);
-            formattedSubEvents.push({ id, fields: plainFields });
+            formattedSubevents.push({ id, fields: plainFields });
         });
-        res.json(formattedSubEvents);
+        res.json(formattedSubevents);
     } catch (error) {
+        console.error("Error Fetching Subevents:", error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+});
+
+// route to get a subevent
+router.get('/subevent/:subevent_id', async (req, res) => {
+    const { subevent_id } = req.params;
+    try {
+        const subevent = await getRecord(SubeventTable, subevent_id);
+
+        if (!subevent) {
+            return res.status(404).json({ message: 'Subevent Not Found' });
+        }
+
+        let plainFields = Object.fromEntries(subevent.get(subevent_id));
+        let formattedSubevents: { id: string, fields: any } = {id: subevent_id, fields: plainFields}
+        res.json(formattedSubevents)
+    } catch (error) {
+        console.error("Error Fetching Subevent:", error);
+        res.status(500).json({error: 'Internal Server Error'});
+}
 });
 
 module.exports = router;
