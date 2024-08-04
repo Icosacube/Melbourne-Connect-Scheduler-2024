225a1416caba3d2a9b9bb7ce02ba0821131e71c8
Keahna <khansenferna@student.unimelb.edu.au>
2024-08-04 15:41:34 +1000

    feat: add GET endpoint to fetch sub events for a specific event

--- mcs_backend/src/controller/subEvent.ts
+++ mcs_backend/src/controller/subEvent.ts
@@ -9,17 +9,35 @@ import {
 } from '../models/airtable';
 
 const router = express.Router();
-
 const SubEventTable = String(process.env.SUBEVENT)
 
+// route to get all subevents
 router.get('/sub_events', async (req, res) => {
     try {
+        // fetch subevents
         const subEvents = await getTable(SubEventTable, "");
+        // format subevents
         const formattedSubEvents: { id: string, fields: any }[] = [];
         subEvents.forEach((fields, id) => {
             const plainFields = Object.fromEntries(fields);
             formattedSubEvents.push({ id, fields: plainFields });
-            console.log(`ID: ${id}, Fields:`, plainFields);
+        });
+        res.json(formattedSubEvents);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// route to get subevents for a specific event
+router.get('/sub_events/:event_id', async (req, res) => {
+    const { event_id } = req.params;
+    try {
+        const subEvents = await getTable(SubEventTable, event_id);
+
+        const formattedSubEvents: { id: string, fields: any }[] = [];
+        subEvents.forEach((fields, id) => {
+            const plainFields = Object.fromEntries(fields);
+            formattedSubEvents.push({ id, fields: plainFields });
         });
         res.json(formattedSubEvents);
     } catch (error) {
