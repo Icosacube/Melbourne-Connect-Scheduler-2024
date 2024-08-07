ecbcc3d3d7bf2a72385a5d6ce485edbe410ec651
Quang Nguyen <quanganhphucnguyen@gmail.com>
2024-08-07 18:52:59 +1000

    fix undefined error

--- frontend/mcs-frontend/src/pages/Event/EventView/TabPages/About.tsx
+++ frontend/mcs-frontend/src/pages/Event/EventView/TabPages/About.tsx
@@ -1,10 +1,10 @@
-import { Avatar, Box, Chip, Stack, Typography } from '@mui/material';
-import React, { FC } from 'react';
-import { MainEvent, Speaker } from '../../../../types/frontendTypes';
-import AboutTable from './AboutTable';
-import Banner from './Banner';
-import Headline from './Headline';
-import { s } from '@fullcalendar/core/internal-common';
+import { Avatar, Box, Chip, Stack, Typography } from "@mui/material";
+import React, { FC } from "react";
+import { MainEvent, Speaker } from "../../../../types/frontendTypes";
+import AboutTable from "./AboutTable";
+import Banner from "./Banner";
+import Headline from "./Headline";
+import { s } from "@fullcalendar/core/internal-common";
 
 interface AboutProps {
   event: MainEvent;
@@ -19,12 +19,12 @@ interface NameCardProps {
 
 const NameCard: FC<NameCardProps> = ({ firstName, lastName, position }) => {
   return (
-    <Box className='pl-4 pr-4 flex space-x-6 bg-white mt-4 pt-4 pb-2 shadow-lg rounded-md'>
-      <Avatar className='size-24 mb-4 ' />
+    <Box className="pl-4 pr-4 flex space-x-6 bg-white mt-4 pt-4 pb-2 shadow-lg rounded-md">
+      <Avatar className="size-24 mb-4 " />
       <Stack>
-        <Typography variant='h5'>{firstName}</Typography>
-        <Typography variant='h5'>{lastName}</Typography>
-        <Typography variant='h6' className='text-gray-400'>
+        <Typography variant="h5">{firstName}</Typography>
+        <Typography variant="h5">{lastName}</Typography>
+        <Typography variant="h6" className="text-gray-400">
           {position}
         </Typography>
       </Stack>
@@ -36,13 +36,13 @@ export const About: FC<AboutProps> = ({ event, speakers }) => {
   const firstSpeaker = speakers[0];
   const theRestOfSpeakers = speakers.slice(1);
   return (
-    <Box className='flex justify-between space-x-8 mt-5'>
+    <Box className="flex justify-between space-x-8 mt-5">
       {/* Left side */}
-      <Box className='w-9/12 space-y-5 bg-white'>
-        <Banner image={'ADD ACTUAL IMAGE INFO'} />
-        <Box className='pl-8 pr-8'>
+      <Box className="w-9/12 space-y-5 bg-white">
+        <Banner image={"ADD ACTUAL IMAGE INFO"} />
+        <Box className="pl-8 pr-8">
           {/* Date and Status */}
-          <Headline date={event.Date?.toString()} name={event.EventName} />
+          <Headline date={event?.Date.toString()} name={event?.EventName} />
 
           {/* Info Table */}
           <AboutTable event={event} />
@@ -50,26 +50,26 @@ export const About: FC<AboutProps> = ({ event, speakers }) => {
       </Box>
 
       {/* Right side */}
-      <Box className='w-3/12'>
-        <Box className=' bg-white rounded-2xl shadow-lg pt-8 pb-8 h-max'>
-          <Box className='pl-8 pr-4 flex space-x-6 '>
-            <Avatar className='size-24 mb-4 ' />
+      <Box className="w-3/12">
+        <Box className=" bg-white rounded-2xl shadow-lg pt-8 pb-8 h-max">
+          <Box className="pl-8 pr-4 flex space-x-6 ">
+            <Avatar className="size-24 mb-4 " />
             <Stack>
-              <Typography variant='h5'>{firstSpeaker.FirstName}</Typography>
-              <Typography variant='h5'>{firstSpeaker.LastName}</Typography>
-              <Typography variant='h6' className='text-gray-400'>
+              <Typography variant="h5">{firstSpeaker.FirstName}</Typography>
+              <Typography variant="h5">{firstSpeaker.LastName}</Typography>
+              <Typography variant="h6" className="text-gray-400">
                 {firstSpeaker.Organisation}
               </Typography>
             </Stack>
           </Box>
           {/* Bio */}
           <Typography
-            variant='body1'
-            className='pl-8 pr-4 overflow-hidden'
+            variant="body1"
+            className="pl-8 pr-4 overflow-hidden"
             sx={{
-              display: '-webkit-box',
-              overflow: 'hidden',
-              WebkitBoxOrient: 'vertical',
+              display: "-webkit-box",
+              overflow: "hidden",
+              WebkitBoxOrient: "vertical",
               WebkitLineClamp: 9,
             }}
           >
@@ -78,9 +78,9 @@ export const About: FC<AboutProps> = ({ event, speakers }) => {
         </Box>
 
         {/* Attendees */}
-        <Box className='flex pl-6 mt-4 space-x-4'>
-          <Typography variant='h5'>Attendees</Typography>
-          <Chip label='64' />
+        <Box className="flex pl-6 mt-4 space-x-4">
+          <Typography variant="h5">Attendees</Typography>
+          <Chip label="64" />
         </Box>
         {theRestOfSpeakers.map((speaker) => (
           <NameCard
