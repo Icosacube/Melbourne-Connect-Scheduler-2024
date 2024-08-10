e62a455d64e63bd3ca6db6a3dbe899ae92505493
Keahna <khansenferna@student.unimelb.edu.au>
2024-08-10 22:00:11 +1000

    update services page to display catering for an event

--- frontend/mcs-frontend/src/pages/Event/EventView/TabPages/Services.tsx
+++ frontend/mcs-frontend/src/pages/Event/EventView/TabPages/Services.tsx
@@ -3,8 +3,11 @@ import React, {FC, useEffect, useState} from 'react';
 import {Catering, MainEvent, FundingAccount} from "../../../../types/frontendTypes";
 import {getCateringByEventID} from "../../../../scripts/catering/functions";
 import Headline from "./Headline";
-import {Box} from "@mui/material";
+import {Box, Button, Typography} from "@mui/material";
 import {getFundingAccounts} from "../../../../scripts/fundingAccount/functions";
+import AddCircleOutlineOutlined from "@mui/icons-material/AddCircleOutlineOutlined";
+import {CreateCateringModal} from "./CreateCateringModal";
+import dayjs from "dayjs";
 
 interface ServicesProps {
     event: MainEvent;
@@ -45,6 +48,10 @@ export const Services: FC<ServicesProps> = ({event}) => {
         fetchData();
     }, [event.RecordID]);
 
+    const handleAddCatering = (newCatering: Catering) => {
+        setCatering((prevCatering) => [...prevCatering, newCatering]);
+    };
+
     function getRowId(catering: Catering) {
         return catering.RecordID;
     }
@@ -68,9 +75,19 @@ export const Services: FC<ServicesProps> = ({event}) => {
             headerClassName: 'services-table',
             flex: 1,
             valueFormatter: (params) => {
-                const value = params as number;
-                return `$${value.toFixed(2)}`; // Format the number with 2 decimal places
-            }
+                const value = params as number; // Access the value correctly
+                return `$${value.toFixed(2)}`; // Format the number with 2 decimal places and add the $ symbol
+            },
+        },
+        {
+            field: 'ExpenseDate',
+            headerName: 'Expense Date',
+            headerClassName: 'services-table',
+            flex: 1,
+            valueFormatter: (params) => {
+                const date = new Date(params);
+                return dayjs(date).format('DD MMM YYYY');
+            },
         },
         {
             field: 'FundingAccount',
@@ -84,31 +101,52 @@ export const Services: FC<ServicesProps> = ({event}) => {
         }
     ];
 
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <Box>
-            <Headline/>
-            <DataGrid
-                rows={catering}
-                columns={columns}
-                getRowId={getRowId}
-                autoHeight
-                pageSizeOptions={[5, 10]}
-                initialState={{
-                    pagination: {
-                        paginationModel: {page: 0, pageSize: 10},
-                    },
-                }}
-                checkboxSelection
-                sx={{
-                    '& .services-table': {
-                        backgroundColor: '#FBE418',
-                        color: 'black',
-                    },
-                    '.MuiDataGrid-columnHeaderTitleContainer': {
-                        backgroundColor: '#FBE418',
-                    },
-                }}
-            />
-        </Box>
+        <>
+            <Box className='  mb-4 flex flex-col'>
+                <Box className=' flex flex-col'>
+                    <Button
+                        variant='contained'
+                        className=' flex space-x-2 bg-secondary hover:bg-accent hover:text-black mb-3 self-end h-12'
+                        onClick={handleOpen}
+                    >
+                        <AddCircleOutlineOutlined/>
+                        <Typography>Create Catering Entry</Typography>
+                    </Button>
+                    <CreateCateringModal open={open} handleClose={handleClose} eventID={event.RecordID}
+                                         fundingAccounts={fundingAccountMap}
+                                         addCatering={handleAddCatering} />
+                </Box>
+                <Box>
+                    <DataGrid
+                        rows={catering}
+                        columns={columns}
+                        getRowId={getRowId}
+                        autoHeight
+                        pageSizeOptions={[5, 10]}
+                        initialState={{
+                            pagination: {
+                                paginationModel: {page: 0, pageSize: 10},
+                            },
+                        }}
+                        checkboxSelection
+                        sx={{
+                            '& .services-table': {
+                                backgroundColor: '#FBE418',
+                                color: 'black',
+                            },
+                            '.MuiDataGrid-columnHeaderTitleContainer': {
+                                backgroundColor: '#FBE418',
+                            },
+                        }}
+                    />
+                </Box>
+            </Box>
+        </>
+
     );
 };
