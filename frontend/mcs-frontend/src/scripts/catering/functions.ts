bedf6b0b791b53c64a30829354aee484f21c9201
Keahna <khansenferna@student.unimelb.edu.au>
2024-08-10 22:00:55 +1000

    add function create catering entry

--- frontend/mcs-frontend/src/scripts/catering/functions.ts
+++ frontend/mcs-frontend/src/scripts/catering/functions.ts
@@ -1,5 +1,6 @@
 import axios, {AxiosResponse} from 'axios';
 import {Catering} from '../../types/frontendTypes';
+import dayjs, {Dayjs} from "dayjs";
 
 // Function to reformat catering response data
 function reformatCateringResponseData(data: any): Catering {
@@ -10,8 +11,10 @@ function reformatCateringResponseData(data: any): Catering {
         BookingReference: data.BookingReference || defaultCatering.BookingReference,
         Description: data.Description || defaultCatering.Description,
         Cost: data.Cost || defaultCatering.Cost,
+        ExpenseDate: data.ExpenseDate ? dayjs(data.ExpenseDate) : defaultCatering.ExpenseDate,
         FundingAccount: data.FundingAccount || defaultCatering.FundingAccount,
         MainEvent: data.MainEvent || defaultCatering.MainEvent,
+        Finance: data.Finance || defaultCatering.Finance,
     };
     return catering;
 }
@@ -19,11 +22,13 @@ function reformatCateringResponseData(data: any): Catering {
 // Default catering object
 export const defaultCatering: Catering = {
     RecordID: '',
-    BookingReference: 0,
+    BookingReference: '',
     Description: '',
     Cost: 0,
-    FundingAccount: '',
-    MainEvent: '',
+    ExpenseDate: dayjs(),
+    FundingAccount: [],
+    MainEvent: [],
+    Finance: [],
 };
 
 // Function to get all catering
@@ -40,5 +45,24 @@ export async function getCateringByEventID(id: string): Promise<Catering[]> {
     } catch (error) {
         console.error('Error fetching catering:', error);
         return [];
+    }
+}
+
+// Function to create catering entry
+export async function createCatering(catering: Catering, id: string): Promise<AxiosResponse> {
+    try {
+        const toSend: any = { ...catering ,
+            ExpenseDate: dayjs(catering.ExpenseDate).format('YYYY-MM-DD') ,
+            Cost: parseFloat(String(catering.Cost)) };
+        delete toSend.RecordID;
+        console.log(toSend);
+        const res = await axios.post(
+            `${process.env.REACT_APP_BACKEND_URL}/catering/${id}`,
+            toSend,
+        );
+        return res;
+    } catch (error) {
+        console.error('Error creating catering:', error);
+        throw error;
     }
 }
