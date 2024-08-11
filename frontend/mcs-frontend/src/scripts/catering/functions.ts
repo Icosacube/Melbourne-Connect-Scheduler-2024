9fb08a86d9d52282608aeeeea7aee528e42060e4
Quang Nguyen <quanganhphucnguyen@gmail.com>
2024-08-11 14:54:06 +1000

    enforce eslint

--- frontend/mcs-frontend/src/scripts/catering/functions.ts
+++ frontend/mcs-frontend/src/scripts/catering/functions.ts
@@ -1,22 +1,24 @@
-import axios, {AxiosResponse} from 'axios';
-import {Catering} from '../../types/frontendTypes';
-import dayjs, {Dayjs} from "dayjs";
+import axios, { AxiosResponse } from 'axios'
+import { Catering } from '../../types/frontendTypes'
+import dayjs from 'dayjs'
 
 // Function to reformat catering response data
 function reformatCateringResponseData(data: any): Catering {
-    const catering: Catering
-        = {
+    const catering: Catering = {
         ...defaultCatering,
         RecordID: data.id || defaultCatering.RecordID,
-        BookingReference: data.BookingReference || defaultCatering.BookingReference,
+        BookingReference:
+            data.BookingReference || defaultCatering.BookingReference,
         Description: data.Description || defaultCatering.Description,
         Cost: data.Cost || defaultCatering.Cost,
-        ExpenseDate: data.ExpenseDate ? dayjs(data.ExpenseDate) : defaultCatering.ExpenseDate,
+        ExpenseDate: data.ExpenseDate
+            ? dayjs(data.ExpenseDate)
+            : defaultCatering.ExpenseDate,
         FundingAccount: data.FundingAccount || defaultCatering.FundingAccount,
         MainEvent: data.MainEvent || defaultCatering.MainEvent,
         Finance: data.Finance || defaultCatering.Finance,
-    };
-    return catering;
+    }
+    return catering
 }
 
 // Default catering object
@@ -29,40 +31,45 @@ export const defaultCatering: Catering = {
     FundingAccount: [],
     MainEvent: [],
     Finance: [],
-};
+}
 
 // Function to get all catering
 export async function getCateringByEventID(id: string): Promise<Catering[]> {
     try {
         const res = await axios.get(
-            `${process.env.REACT_APP_BACKEND_URL}/catering/${id}`,
-        );
-        const rawCatering = res.data;
+            `${process.env.REACT_APP_BACKEND_URL}/catering/${id}`
+        )
+        const rawCatering = res.data
         const formattedCatering = rawCatering.map((catering: any) =>
-            reformatCateringResponseData(catering),
-        );
-        return formattedCatering;
+            reformatCateringResponseData(catering)
+        )
+        return formattedCatering
     } catch (error) {
-        console.error('Error fetching catering:', error);
-        return [];
+        console.error('Error fetching catering:', error)
+        return []
     }
 }
 
 // Function to create catering entry
-export async function createCatering(catering: Catering, id: string): Promise<AxiosResponse> {
+export async function createCatering(
+    catering: Catering,
+    id: string
+): Promise<AxiosResponse> {
     try {
-        const toSend: any = { ...catering ,
-            ExpenseDate: dayjs(catering.ExpenseDate).format('YYYY-MM-DD') ,
-            Cost: parseFloat(String(catering.Cost)) };
-        delete toSend.RecordID;
-        console.log(toSend);
+        const toSend: any = {
+            ...catering,
+            ExpenseDate: dayjs(catering.ExpenseDate).format('YYYY-MM-DD'),
+            Cost: parseFloat(String(catering.Cost)),
+        }
+        delete toSend.RecordID
+        console.log(toSend)
         const res = await axios.post(
             `${process.env.REACT_APP_BACKEND_URL}/catering/${id}`,
-            toSend,
-        );
-        return res;
+            toSend
+        )
+        return res
     } catch (error) {
-        console.error('Error creating catering:', error);
-        throw error;
+        console.error('Error creating catering:', error)
+        throw error
     }
 }
