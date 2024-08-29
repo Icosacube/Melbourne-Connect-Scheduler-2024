8aec87c669fd92c734a5702563c2b1ba31bc0be6
Phuc Quang Anh Nguyen <quanganhphucnguyen@gmail.com>
2024-08-29 13:31:46 +1000

    revamp mui grid toolbar

--- frontend/mcs-frontend/src/components/DataGrid/CustomToolBar.tsx
+++ frontend/mcs-frontend/src/components/DataGrid/CustomToolBar.tsx
@@ -8,15 +8,10 @@ import {
     GridToolbarFilterButton,
     useGridApiContext,
 } from '@mui/x-data-grid'
-import { createSvgIcon } from '@mui/material/utils'
+import FileDownloadIcon from '@mui/icons-material/FileDownload'
 
 const getRowsFromCurrentPage = ({ apiRef }: GridCsvGetRowsToExportParams) =>
     gridPaginatedVisibleSortedGridRowIdsSelector(apiRef)
-
-const ExportIcon = createSvgIcon(
-    <path d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2z" />,
-    'SaveAlt'
-)
 
 export const CustomToolbar = () => {
     const apiRef = useGridApiContext()
@@ -27,7 +22,7 @@ export const CustomToolbar = () => {
     const buttonBaseProps: ButtonProps = {
         color: 'primary',
         size: 'small',
-        startIcon: <ExportIcon />,
+        startIcon: <FileDownloadIcon />,
     }
 
     return (
