bbc70330394de645cda7179a24738e043e074a1a
Kia Sen Tan <kiat@student.unimelb.edu.au>
2024-09-04 21:46:53 +1000

    updated to store JWT token as cookie

--- frontend/mcs-frontend/src/scripts/authentication/login.tsx
+++ frontend/mcs-frontend/src/scripts/authentication/login.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import cookie from 'cookie'
 
 interface loginResponse {
     username: string
@@ -14,11 +15,44 @@ export const login = async (username: string, password: string) => {
 
     if (res.status != 200) {
         // login failed
+        console.log('login failed')
         return
     }
     const data = res.data as loginResponse
-    console.log("successfully logged in!")
+    console.log('successfully logged in!')
     console.log(data.token)
 
-    localStorage.setItem('session', data.token)
+    //Login token expires after 15 minutes
+    var date = new Date()
+    date.setTime(date.getTime() + 15 * 60 * 1000)
+
+    const loginCookie = cookie.serialize('login', data.token, {
+        expires: date,
+    })
+    document.cookie = loginCookie
+}
+
+// refreshes the token provided we have it
+export const refresh = async () => {
+    const res = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}${process.env.REACT_APP_LOGIN_REFRESH_API_PATH}`
+    )
+
+    if (res.status != 200) {
+        // login failed
+        console.log('login failed')
+        return
+    }
+    const data = res.data as loginResponse
+    console.log('successfully logged in!')
+    console.log(data.token)
+
+    //Login token expires after 15 minutes
+    var date = new Date()
+    date.setTime(date.getTime() + 15 * 60 * 1000)
+
+    const refreshCookie = cookie.serialize('login', data.token, {
+        expires: date,
+    })
+    document.cookie = refreshCookie
 }
