63967343378f867f3ffdca349fc14b8cca24a367
Phuc Quang Anh Nguyen <quanganhphucnguyen@gmail.com>
2024-09-12 21:14:54 +1000

    refractor share event function

--- frontend/mcs-frontend/src/pages/Event/EventView/ShareEventModal.tsx
+++ frontend/mcs-frontend/src/pages/Event/EventView/ShareEventModal.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import 'react-quill/dist/quill.snow.css'
 import { EmailComposerModal } from '../../../components'
 import { MainEvent, Speaker } from '../../../types/frontendTypes'
+import { generateHtmlEmailTemplateFromEvents } from '../../../scripts/email/functions'
 
 interface ShareEventModalProps {
     isOpen: boolean
@@ -11,65 +12,13 @@ interface ShareEventModalProps {
     speaker: Speaker
 }
 
-function generateHtmlEmailTemplate(event: MainEvent, speaker: Speaker) {
-    console.log(speaker)
-
-    // Format the date using dayjs
-    const formattedDate = event.Date.format('dddd, MMMM D, YYYY')
-    const formattedTime = event.Date.format('h:mm A')
-    const speakerDetails = [
-        speaker.WorkTitle,
-        speaker.Department,
-        speaker.Organisation,
-    ]
-        .filter(Boolean)
-        .join(' | ')
-
-    const emailSubject = `INVITATION: ${event.EventName} | ${formattedDate}`
-
-    const emailContent = `
-        <html>
-        <body style="font-family: Arial, sans-serif; line-height: 1.6;">
-            <img src="${
-                event.EventBanner[0]?.url
-            }" alt="Event Banner" style="max-width: 100%;">
-            <h1><b>${event.EventName}</b></h1>
-            <p>${event.EventAbstract}</p>
-            <br/>
-            <h1><b>About the Speaker</b></h1>
-            <img src="${speaker.Headshot[0]?.url}" alt="${speaker.FirstName} ${
-        speaker.LastName
-    }" style="max-width: 20px;"><br>
-            <h2><b>${speaker.Title} ${speaker.FirstName} ${
-        speaker.LastName
-    }</b></h2>
-            ${speakerDetails ? `<h3>${speakerDetails}</h3>` : ''}
-            <p>${speaker.Bio}</p>
-            <br/>
-            <h2><b>Event Details:</b></h2>
-            <p>${event.EventDescription}</p>
-            <ul>
-                <li><strong>Date:</strong> ${formattedDate}</li>
-                <li><strong>Time:</strong> ${formattedTime}</li>
-            </ul>
-            <br/>
-            <p>We look forward to seeing you at the event!</p>
-            
-            <p>Best regards,<br>[Your Name]</p>
-        </body>
-        </html>
-    `
-
-    return { emailSubject, emailContent }
-}
-
 export const ShareEventModal: React.FC<ShareEventModalProps> = ({
     isOpen,
     onClose,
     event,
     speaker,
 }) => {
-    const { emailSubject, emailContent } = generateHtmlEmailTemplate(
+    const { emailSubject, emailContent } = generateHtmlEmailTemplateFromEvents(
         event,
         speaker
     )
--- frontend/mcs-frontend/src/scripts/email/functions.ts
+++ frontend/mcs-frontend/src/scripts/email/functions.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse, AxiosError } from 'axios'
+import { MainEvent, Speaker } from '../../types/frontendTypes'
 
 export async function sendEmail(
     from: string,
@@ -44,4 +45,59 @@ export async function getBlankSpeakerEventFormLink() {
 
 export async function getExistingSpeakerEventFormLink() {
     return 'google.com'
+}
+
+export function generateHtmlEmailTemplateFromEvents(
+    event: MainEvent,
+    speaker: Speaker
+) {
+    console.log(speaker)
+
+    // Format the date using dayjs
+    const formattedDate = event.Date.format('dddd, MMMM D, YYYY')
+    const formattedTime = event.Date.format('h:mm A')
+    const speakerDetails = [
+        speaker.WorkTitle,
+        speaker.Department,
+        speaker.Organisation,
+    ]
+        .filter(Boolean)
+        .join(' | ')
+
+    const emailSubject = `INVITATION: ${event.EventName} | ${formattedDate}`
+
+    const emailContent = `
+        <html>
+        <body style="font-family: Arial, sans-serif; line-height: 1.6;">
+            <img src="${
+                event.EventBanner[0]?.url
+            }" alt="Event Banner" style="max-width: 100%;">
+            <h1><b>${event.EventName}</b></h1>
+            <p>${event.EventAbstract}</p>
+            <br/>
+            <h1><b>About the Speaker</b></h1>
+            <img src="${speaker.Headshot[0]?.url}" alt="${speaker.FirstName} ${
+        speaker.LastName
+    }" style="max-width: 20px;"><br>
+            <h2><b>${speaker.Title} ${speaker.FirstName} ${
+        speaker.LastName
+    }</b></h2>
+            ${speakerDetails ? `<h3>${speakerDetails}</h3>` : ''}
+            <p>${speaker.Bio}</p>
+            <br/>
+            <h2><b>Event Details:</b></h2>
+            <p>${event.EventDescription}</p>
+            <ul>
+                <li><strong>Date:</strong> ${formattedDate}</li>
+                <li><strong>Time:</strong> ${formattedTime}</li>
+            </ul>
+            <br/>
+            <p>We look forward to seeing you at the event!</p>
+            
+            <p>Best regards,<br>[Your Name]</p>
+        </body>
+        </html>
+    `
+
+    return { emailSubject, emailContent }
 }
