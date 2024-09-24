a3b30233311ce717e10f91b642be30cc54c2be37
pH7uang <pH7uang@gmail.com>
2024-09-24 17:08:23 +1000

    improve porgramme calendar layout

--- frontend/mcs-frontend/src/components/Calendar/WeeklyCalendar.tsx
+++ frontend/mcs-frontend/src/components/Calendar/WeeklyCalendar.tsx
@@ -4,68 +4,71 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import { MainEvent } from '../../types/frontendTypes'
 import { SubEvent } from '../../types/frontendTypes'
+import dayjs from 'dayjs'
 
 interface WeeklyCalendarProps {
-  event: MainEvent,
-  subEvents: SubEvent[],
-  onEventClick: (subEvent: SubEvent) => void
+    event: MainEvent
+    subEvents: SubEvent[]
+    onEventClick: (subEvent: SubEvent) => void
 }
 
 const WeeklyCalendar: React.FC<WeeklyCalendarProps> = ({
-  event,
-  subEvents,
-  onEventClick,
-  }) => {
-  const [allEvents, setAllEvents] = useState<any[]>([]);
+    event,
+    subEvents,
+    onEventClick,
+}) => {
+    const [allEvents, setAllEvents] = useState<any[]>([])
 
-  const mainEventProp = {
-    id: `main-${event.RecordID}`, // identifier of main events  
-    title: event.EventName,
-    start: event.Date.toDate(),
-    backgroundColor: '#90A4AE', 
-    borderColor: '#90A4AE', 
-  };
+    const mainEventProp = {
+        id: `main-${event.RecordID}`, // identifier of main events
+        title: event.EventName,
+        start: event.Date.toDate(),
+        backgroundColor: '#90A4AE',
+        borderColor: '#90A4AE',
+    }
 
-  //changed 'subevent' to 'subEvent'. Functionality untouched
-  const subEventsProp = subEvents.map((subEvent) => ({
-    id: `sub-${subEvent.RecordID}`, // identifier of sub-events 
-    title: subEvent.EventName,
-    start: subEvent.StartDate.toDate(),
-    end: subEvent.EndDate.toDate(),
-    extendedProps: subEvent,
-  }));
-  
+    //changed 'subevent' to 'subEvent'. Functionality untouched
+    const subEventsProp = subEvents.map((subEvent) => ({
+        id: `sub-${subEvent.RecordID}`, // identifier of sub-events
+        title: subEvent.EventName,
+        start: subEvent.StartDate.toDate(),
+        end: subEvent.EndDate.toDate(),
+        extendedProps: subEvent,
+    }))
+
     useEffect(() => {
         setAllEvents([mainEventProp, ...subEventsProp])
     }, [event, subEvents])
 
-
-  const handleEventClick = (info: any) => {
-    if (info.event.id.startsWith("sub-")) { // Check if the event is a sub-event
-      const subEvent = info.event.extendedProps as SubEvent;
-      onEventClick(subEvent); 
-    } else {
-
+    const handleEventClick = (info: any) => {
+        if (info.event.id.startsWith('sub-')) {
+            // Check if the event is a sub-event
+            const subEvent = info.event.extendedProps as SubEvent
+            onEventClick(subEvent)
+        } else {
+        }
     }
-  }
 
-  return (
-    <FullCalendar
-      plugins={[timeGridPlugin, interactionPlugin]}
-      initialView="timeGridWeek"
-      height="auto"
-      events={allEvents}
-      headerToolbar={{
-        left: "prev,next today",
-        center: "title",
-        right: "timeGridWeek,timeGridDay",
-      }}
-      slotMinTime="08:00:00"
-      slotMaxTime="20:00:00"
-      locale="en-GB"
-      eventClick={handleEventClick}
-    />
-  );
-};
+    return (
+        <FullCalendar
+            plugins={[timeGridPlugin, interactionPlugin]}
+            initialView="timeGridWeek"
+            height="auto"
+            allDaySlot={false}
+            events={allEvents}
+            initialDate={dayjs(event.Date).format('YYYY-MM-DD')}
+            headerToolbar={{
+                left: 'title',
+                center: '',
+                right: 'today prev,next timeGridWeek,timeGridDay',
+            }}
+            titleFormat={{ year: 'numeric', month: 'short' }}
+            slotMinTime="08:00:00"
+            slotMaxTime="20:00:00"
+            locale="en-GB"
+            eventClick={handleEventClick}
+        />
+    )
+}
 
 export default WeeklyCalendar
