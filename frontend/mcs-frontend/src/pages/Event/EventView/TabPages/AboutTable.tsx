38e932ef7bde11699d4761ec429a639d398ac026
pH7uang <pH7uang@gmail.com>
2024-09-24 16:35:35 +1000

    fix the event about page resizeObserver bug

--- frontend/mcs-frontend/src/pages/Event/EventView/TabPages/AboutTable.tsx
+++ frontend/mcs-frontend/src/pages/Event/EventView/TabPages/AboutTable.tsx
@@ -13,25 +13,27 @@ interface CustomTextAreaProps {
     minRows?: number
 }
 
-const CustomTextArea: FC<CustomTextAreaProps> = ({ title, text, minRows }) => {
+const CustomTextArea: FC<CustomTextAreaProps> = ({ title, text }) => {
     return (
-        <Stack>
+        <Stack spacing={1}>
             <Typography variant="subtitle1" className="text-gray-400">
                 {title}
             </Typography>
-            <TextField
-                disabled
-                minRows={minRows}
-                multiline
-                defaultValue={text}
+            <Box
                 sx={{
-                    '& .MuiInputBase-input.Mui-disabled': {
-                        WebkitTextFillColor: '#000000',
-                    },
+                    backgroundColor: '#EFF0F1',
+                    padding: '12px',
+                    borderRadius: '8px',
+                    width: '100%',
                     maxHeight: '800px',
-                    overflow: 'auto',
+                    lineHeight: '1.2',
+                    overflowY: 'auto', // Scroll
+                    color: '#000000',
+                    whiteSpace: 'pre-line',
                 }}
-            />
+            >
+                <Typography variant="body1">{text || 'N/A'}</Typography>
+            </Box>
         </Stack>
     )
 }
@@ -50,21 +52,24 @@ export const AboutTable: FC<AboutTableProps> = ({ event, venues }) => {
 
     return (
         <Grid container spacing={3}>
-            <Grid item xs={12} md={6}>
-                <CustomTextArea
-                    title="Venue"
-                    text={venueFormatted}
-                    minRows={1}
-                />
-
-                <CustomTextArea
-                    title="Event Description"
-                    text={event?.EventDescription}
-                    minRows={5}
-                />
+            <Grid item xs={12} lg={6} container spacing={2}>
+                <Grid item xs={12}>
+                    <CustomTextArea
+                        title="Venue"
+                        text={venueFormatted}
+                        minRows={1}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <CustomTextArea
+                        title="Event Description"
+                        text={event?.EventDescription}
+                        minRows={5}
+                    />
+                </Grid>
             </Grid>
 
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} lg={6}>
                 <CustomTextArea
                     title="Talk Abstract"
                     text={event?.EventAbstract}
