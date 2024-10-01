26398ed3c6c53a53284f8bad8413d58507183d21
Phuc Quang Anh Nguyen <quanganhphucnguyen@gmail.com>
2024-10-01 18:38:02 +1000

    eslint

--- frontend/mcs-frontend/src/components/TopNavBar/PageTopNavBar.tsx
+++ frontend/mcs-frontend/src/components/TopNavBar/PageTopNavBar.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import CreateIcon from '@mui/icons-material/Create'
 import {
     Box,
     Breadcrumbs,
@@ -8,9 +8,8 @@ import {
     Toolbar,
     Typography,
 } from '@mui/material'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import CreateIcon from '@mui/icons-material/Create'
-import ShareIcon from '@mui/icons-material/Share'
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 interface PageTopNavBarProps {
     type: string
